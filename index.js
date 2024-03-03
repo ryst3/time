1490b909701c67bf000f7422364394acd34ecb5f
ryst3 <133981517+ryst3@users.noreply.github.com>
2024-03-03 20:09:04 +0400

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function get_age_in_days()
     const now = Date.parse(new Date());
     const past = Date.parse(new Date(date.value));
 
-    const age_days = parseInt((now - past) / (1000 * 3600 * 24))
+    const age_days = parseInt(Math.abs((now - past) / (1000 * 3600 * 24)))
     const time = Date(now).split(" ")[4].split(":").map(n => n = parseInt(n));
     const time_sec = time[0] * 3600 + time[1] * 60 + time[2];
 
-    dp.innerHTML = `<span id="age-days">${age_days}</span> <span id="time-sec">${time_sec}</span>`
+    dp.innerHTML = `${age_days} ${time_sec}`
 }
